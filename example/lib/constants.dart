3dc120d1255c36dc00ae4f10e05a96540381dff4
Ricardo <ricardo@owqlo.com>
2023-05-19 01:29:28 +0200

    add example project

---
+++ example/lib/constants.dart
@@ -0,0 +1,6 @@
+class Constants {
+  // The AZbox API Key. Create it here https://azbox.io/
+  static const String kAzboxApiKey = 'your_api_key';
+  // The AZbox project ID.
+  static const String kAzboxProject = 'your_project_id';
+}
---
+++ example/lib/main.dart
@@ -0,0 +1,78 @@
+import 'package:example/constants.dart';
+import 'package:flutter/material.dart';
+import 'package:azbox/azbox.dart';
+
+void main() {
+
+  Azbox.ensureInitialized();
+
+  runApp(
+    Azbox(
+      supportedLocales: [Locale('en', 'US'), Locale('de', 'DE'), Locale('es', 'ES')],
+      fallbackLocale: Locale('es', 'ES'),
+      apiKey: Constants.kAzboxApiKey,
+      project: Constants.kAzboxProject,
+      child: const MyApp()));
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Azbox Demo'.translate(),
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Azbox Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int _counter = 0;
+
+  void _incrementCounter() {
+    setState(() {
+      _counter++;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text(
+              'You have pushed the button this many times:',
+            ).translate(),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment'.translate(),
+        child: const Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
